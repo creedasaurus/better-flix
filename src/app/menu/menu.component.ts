ea36f577aaf3858f55be3546c907e94ed3490b60
Creed Haymond <creedasaurus@gmail.com>
2017-04-28 14:03:14 -0600

    trying to populate the filter and orderby select options.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,8 +8,25 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 export class MenuBarComponent {
   @Output() onViewChange = new EventEmitter<boolean>();
+  selectedFilter: string = 'rating';
+  selectedSpecFilter: string = 'PG';
+
+  filterOptions: string[];
+  filterSpecOps: string[];
+
+  filters = {
+    'rating': ['G', 'PG'],
+    'review_score': [1,2,3,4,5],
+    'category': ['anime', 'live-action'],
+    'cost-type': ['paid', 'free']
+  };
+
 
   viewChange(cardsView: boolean) {
     this.onViewChange.emit(cardsView);
+  }
+
+  constructor() {
+    this.filterOptions = Object.keys(this.filters);
   }
 }
